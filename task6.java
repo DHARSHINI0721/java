25ad8e50e80ef17cc331942ffa24baf5b2aaa4a9
DHARSHINI0721 <dharshinidhinesh17@gmail.com>
2026-07-23 15:19:23 +0530

    Implement average calculation for array elements
    This program calculates the average of user-inputted elements in an array.

---
+++ task6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) 
+        {
+            arr[i] = sc.nextInt();
+        }
+        double sum=0;
+        for (int i = 0; i < n; i++) 
+        {
+            sum=sum+arr[i];
+        }
+         double avg=sum/n;
+        System.out.println("Average of the elements in array:"+avg );
+    }
+}
