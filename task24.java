a9c534f19faade90108d24fb35959cca9359f46f
DHARSHINI0721 <dharshinidhinesh17@gmail.com>
2026-07-23 20:17:32 +0530

    Implement last occurrence search in an array

---
+++ task24.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.println("Enter element:");
+        int key = sc.nextInt();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (arr[i] == key) {
+                System.out.println("Last Occurrence = " + i);
+                return;
+            }
+        }
+
+        System.out.println("Element not found");
+    }
+}
