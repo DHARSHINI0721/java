6e9371a31377d68344b8fab09a8f9c00716105c3
DHARSHINI0721 <dharshinidhinesh17@gmail.com>
2026-07-23 15:02:19 +0530

    Create task3.java

---
+++ task3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of n:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int max=arr[0];
+        int max2=arr[0];;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+            max2 = max;
+            max = arr[i];
+               } 
+           else if (arr[i] > max2 && arr[i] != max) 
+         {
+             max2 = arr[i];
+          }
+        }
+        
+        System.out.println("max2"+max2);
+}
+}
